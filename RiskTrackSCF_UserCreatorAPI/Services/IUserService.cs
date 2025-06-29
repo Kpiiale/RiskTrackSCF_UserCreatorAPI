3aa401303bef87e96491d4bdf10df5125504339b
Kpiiale <yuliana.capito@udla.edu.ec>
2025-06-29 12:37:10 -0500

    Add service interface for login and registration logic

--- RiskTrackSCF_UserCreatorAPI/Services/IUserService.cs
+++ RiskTrackSCF_UserCreatorAPI/Services/IUserService.cs
@@ -1,6 +1,11 @@
-﻿namespace RiskTrackSCF_UserCreatorAPI.Services
+﻿using RiskTrackSCF_UserCreatorAPI.DTOs;
+using RiskTrackSCF_UserCreatorAPI.Models;
+
+namespace RiskTrackSCF_UserCreatorAPI.Services
 {
     public interface IUserService
     {
+        User? Authenticate(LoginRequest request);
+        void CreateUser(CreateUserRequest request);
     }
 }
