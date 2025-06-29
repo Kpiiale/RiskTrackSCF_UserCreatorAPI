2290d04a8eb4f09d5426ed3a51256a2667d90c0f
Kpiiale <yuliana.capito@udla.edu.ec>
2025-06-29 00:59:58 -0500

    Configure ApplicationDbContext with User DbSet

--- RiskTrackSCF_UserCreatorAPI/Data/ApplicationDbContext.cs
+++ RiskTrackSCF_UserCreatorAPI/Data/ApplicationDbContext.cs
@@ -1,6 +1,20 @@
-﻿namespace RiskTrackSCF_UserCreatorAPI.Data
+﻿using RiskTrackSCF_UserCreatorAPI.Models.UserCreatorApi.Models;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using Microsoft.EntityFrameworkCore;
+
+namespace RiskTrackSCF_UserCreatorAPI.Data
 {
-    public class Class
+    public class ApplicationDbContext : DbContext
     {
+        public DbSet<User> Usuarios { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+     
+        }
     }
 }
