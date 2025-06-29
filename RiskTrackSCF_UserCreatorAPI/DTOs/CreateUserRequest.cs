78678428f136530d972fc4f655700df11ecf6244
Kpiiale <yuliana.capito@udla.edu.ec>
2025-06-29 12:26:45 -0500

    Implement DTO for handling new user creation requests

--- RiskTrackSCF_UserCreatorAPI/DTOs/CreateUserRequest.cs
+++ RiskTrackSCF_UserCreatorAPI/DTOs/CreateUserRequest.cs
@@ -2,5 +2,10 @@
 {
     public class CreateUserRequest
     {
+        public int? CompanyId { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Role { get; set; } = "U";
     }
 }
