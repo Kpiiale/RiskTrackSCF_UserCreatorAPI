1d785176589ed0f8b5572956c36592b20aaf2b38
Kpiiale <yuliana.capito@udla.edu.ec>
2025-06-29 00:29:07 -0500

    Add Model User class

--- RiskTrackSCF_UserCreatorAPI/Models/User.cs
+++ RiskTrackSCF_UserCreatorAPI/Models/User.cs
@@ -1,6 +1,44 @@
-﻿namespace RiskTrackSCF_UserCreatorAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UserCreatorApi.Models
 {
-    public class User
+    namespace UserCreatorApi.Models
     {
+        [Table("Usuario")]
+        public class User
+        {
+            [Key]
+            [Column("id_usuario")]
+            public int Id { get; set; }
+
+            [Column("id_empresa")]
+            public int? CompanyId { get; set; }
+
+            [Column("nombre_completo")]
+            [StringLength(100)]
+            public string? FullName { get; set; }
+
+            [Required]
+            [EmailAddress]
+            [Column("correo")]
+            [StringLength(100)]
+            public string Email { get; set; }
+
+            [Required]
+            [Column("contrasena_hash")]
+            [StringLength(255)]
+            public string PasswordHash { get; set; }
+
+            [Column("rol")]
+            [StringLength(20)]
+            public string Role { get; set; } = "U";
+
+            [Column("fecha_creacion")]
+            public DateTime CreationDate { get; set; } = DateTime.UtcNow;
+
+            [Column("activo")]
+            public bool IsActive { get; set; } = true;
+        }
     }
 }
