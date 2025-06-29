d618ea16a12dba05878cd2ac68b96305a9eacf71
Kpiiale <yuliana.capito@udla.edu.ec>
2025-06-29 12:29:45 -0500

    Fix User model

--- RiskTrackSCF_UserCreatorAPI/Models/User.cs
+++ RiskTrackSCF_UserCreatorAPI/Models/User.cs
@@ -3,42 +3,32 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RiskTrackSCF_UserCreatorAPI.Models
 {
-    namespace UserCreatorApi.Models
+    [Table("Usuario")]
+    public class User
     {
-        [Table("Usuario")]
-        public class User
-        {
-            [Key]
-            [Column("id_usuario")]
-            public int Id { get; set; }
-
-            [Column("id_empresa")]
-            public int? CompanyId { get; set; }
-
-            [Column("nombre_completo")]
-            [StringLength(100)]
-            public string? FullName { get; set; }
-
-            [Required]
-            [EmailAddress]
-            [Column("correo")]
-            [StringLength(100)]
-            public string Email { get; set; }
-
-            [Required]
-            [Column("contrasena_hash")]
-            [StringLength(255)]
-            public string PasswordHash { get; set; }
-
-            [Column("rol")]
-            [StringLength(20)]
-            public string Role { get; set; } = "U";
-
-            [Column("fecha_creacion")]
-            public DateTime CreationDate { get; set; } = DateTime.UtcNow;
-
-            [Column("activo")]
-            public bool IsActive { get; set; } = true;
-        }
+        [Key]
+        [Column("id_usuario")]
+        public int Id { get; set; }
+        [Column("id_empresa")]
+        public int? CompanyId { get; set; }
+        [Column("nombre_completo")]
+        [StringLength(100)]
+        public string? FullName { get; set; }
+        [Required]
+        [EmailAddress]
+        [Column("correo")]
+        [StringLength(100)]
+        public string Email { get; set; }
+        [Required]
+        [Column("contrasena_hash")]
+        [StringLength(255)]
+        public string PasswordHash { get; set; }
+        [Column("rol")]
+        [StringLength(20)]
+        public string Role { get; set; } = "U";
+        [Column("fecha_creacion")]
+        public DateTime CreationDate { get; set; } = DateTime.UtcNow;
+        [Column("activo")]
+        public bool IsActive { get; set; } = true;
     }
 }
