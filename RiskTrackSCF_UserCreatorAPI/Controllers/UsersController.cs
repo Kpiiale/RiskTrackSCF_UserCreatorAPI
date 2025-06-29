f19ff450c5cd9b6758e4a92306f388ad9deae6b9
Kpiiale <yuliana.capito@udla.edu.ec>
2025-06-29 00:08:14 -0500

    Add project files.

---
+++ RiskTrackSCF_UserCreatorAPI/Controllers/UsersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RiskTrackSCF_UserCreatorAPI.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
