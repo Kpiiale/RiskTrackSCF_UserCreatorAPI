3ef3167e2459707bd54f1d5e495f9bf1f3fe4c88
Kpiiale <yuliana.capito@udla.edu.ec>
2025-06-29 00:33:00 -0500

    Fix namespace

--- RiskTrackSCF_UserCreatorAPI/Models/User.cs
+++ RiskTrackSCF_UserCreatorAPI/Models/User.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace UserCreatorApi.Models
+namespace RiskTrackSCF_UserCreatorAPI.Models
 {
     namespace UserCreatorApi.Models
     {
