8c9c9042e5b3c7afe6a75dd63dd9cb826b567498
Kpiiale <yuliana.capito@udla.edu.ec>
2025-07-05 22:49:18 -0500

    Fix AppDbContext to match the new model

--- RiskTrackSCF_UserCreatorAPI/Data/ApplicationDbContext.cs
+++ RiskTrackSCF_UserCreatorAPI/Data/ApplicationDbContext.cs
@@ -7,7 +7,8 @@ namespace RiskTrackSCF_UserCreatorAPI.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public DbSet<User> Usuarios { get; set; }
+        public DbSet<User> Users { get; set; }
+
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options) { }
