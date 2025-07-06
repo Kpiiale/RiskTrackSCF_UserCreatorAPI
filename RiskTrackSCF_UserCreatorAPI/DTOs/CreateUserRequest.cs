6d414552ece2ac3c0981c112e26153277b3aafa5
Kpiiale <yuliana.capito@udla.edu.ec>
2025-07-05 22:56:50 -0500

    Fix CreateUserRequest

--- RiskTrackSCF_UserCreatorAPI/DTOs/CreateUserRequest.cs
+++ RiskTrackSCF_UserCreatorAPI/DTOs/CreateUserRequest.cs
@@ -3,9 +3,9 @@
     public class CreateUserRequest
     {
         public int? CompanyId { get; set; }
-        public string FullName { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
-        public string Role { get; set; } = "U";
+        public string Role { get; set; } = "U"; // Default role
     }
 }
