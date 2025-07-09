565c00078dda521631e042476e80255c84ff9406
Kpiiale <yuliana.capito@udla.edu.ec>
2025-07-09 01:35:03 -0500

    Add CompaniesController

---
+++ RiskTrackSCF_UserCreatorAPI/Controllers/CompaniesController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RiskTrackSCF_UserCreatorAPI.Data;
+using RiskTrackSCF_UserCreatorAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace RiskTrackSCF_UserCreatorAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompaniesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CompaniesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCompany([FromBody] Company company)
+        {
+            _context.Companies.Add(company);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetCompany), new { id = company.CompanyId }, company);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCompany(int id)
+        {
+            var company = await _context.Companies.FindAsync(id);
+            if (company == null)
+                return NotFound();
+
+            return Ok(company);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCompanies()
+        {
+            return Ok(await _context.Companies.ToListAsync());
+        }
+    }
+}
