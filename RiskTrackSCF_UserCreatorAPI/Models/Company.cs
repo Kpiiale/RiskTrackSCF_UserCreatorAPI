3a3fa1192e2c0a93bd2bb6f11c04db6410ce6397
Kpiiale <yuliana.capito@udla.edu.ec>
2025-07-10 15:43:55 -0500

    Fix Models

--- RiskTrackSCF_UserCreatorAPI/Models/Company.cs
+++ RiskTrackSCF_UserCreatorAPI/Models/Company.cs
@@ -1,4 +1,6 @@
-﻿namespace RiskTrackSCF_UserCreatorAPI.Models
+﻿using System.Text.Json.Serialization;
+
+namespace RiskTrackSCF_UserCreatorAPI.Models
 {
     public class Company
     {
@@ -6,6 +8,8 @@
         public string? Name { get; set; }
         public string? RUC { get; set; }
         public string? Sector { get; set; }
+
+        [JsonIgnore]
         public ICollection<User>? Users { get; set; } 
     }
 }
